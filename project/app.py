2421734af2a825e8eba7a6d0013a3b0b5c6a2405
nullxed <definenx0204@gmail.com>
2025-06-27 02:04:38 +0800

    - complete func `train` and `eval`. - update app.py to make sure files in directory `project` can find module `minitorch` - update site-packages/streamlit/elements/image.py and arrow_altair.py to make the version conflicts disappears(but i didn't upload them, btw my python is 3.13)

--- project/app.py
+++ project/app.py
@@ -1,4 +1,10 @@
 from argparse import ArgumentParser
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root_dir = os.path.join(current_dir, '..')
+sys.path.insert(0, project_root_dir)
 
 import streamlit as st
 from interface.streamlit_utils import get_img_tag
